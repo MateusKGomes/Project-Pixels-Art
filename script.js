e18ec4b70c83c37f4e8a0af741927746f93194af
Mateus Kulik <mateuskulik30@gmail.com>
2022-10-04 18:08:43 -0300

    Adiciona quarto requisito

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+// Requisito 1 e 2:
 function createDiv () {
 let colorPallet = document.getElementById('color-palette');
 colorPallet.style.height = '1px';
@@ -16,30 +18,74 @@ for (let index = 1; index <=4; index += 1){
 }
 }
 createDiv();
-
-function color1 () {
+// Requisito 3:
+function color1 (color) {
     let border = document.getElementById('color1');
-    border.style.backgroundColor = 'black';
+    border.style.backgroundColor = color;
+
 
 }
-color1();
+color1('black');
 
-function color2 () {
+function color2 (color) {
     let border = document.getElementById('color2');
-    border.style.backgroundColor = 'blue';
+    border.style.backgroundColor = color;
 
 }
-color2();
-function color3 () {
+color2('blue');
+function color3 (color) {
     let border = document.getElementById('color3');
-    border.style.backgroundColor = 'green';
+    border.style.backgroundColor = color;
 
 }
-color3();
+color3('green');
 
-function color4 () {
+function color4 (color) {
     let border = document.getElementById('color4');
-    border.style.backgroundColor = 'salmon';
+    border.style.backgroundColor = color;
+    
 
 }
-color4();
+color4('red');
+
+function buttonColor () {
+ let randomButton = document.createElement('button');
+ randomButton.id = 'button-random-color';
+randomButton.innerHTML = 'Cores aleatórias'
+document.body.appendChild(randomButton);
+}
+buttonColor();
+
+function randomColor2 (){
+let colorGenerate = document.getElementById('button-random-color');
+randomGenerate =  Math.floor(Math.random()*16777215).toString(16);
+let color2 = document.getElementById('color2');
+color2.style.backgroundColor= "#" + randomGenerate;
+
+
+    colorGenerate.addEventListener("click", randomColor2);
+
+}
+randomColor2();
+
+function randomColor3 (){
+let colorGenerate = document.getElementById('button-random-color');
+randomGenerate =  Math.floor(Math.random()*16777215).toString(16);
+let color3 = document.getElementById('color3');
+color3.style.backgroundColor= "#" + randomGenerate;
+colorGenerate.addEventListener("click", randomColor3);
+
+}
+randomColor3();
+
+
+function randomColor4 (){
+    let colorGenerate = document.getElementById('button-random-color');
+    randomGenerate =  Math.floor(Math.random()*16777215).toString(16);
+    let color4 = document.getElementById('color4');
+    color4.style.backgroundColor= "#" + randomGenerate;
+    colorGenerate.addEventListener("click", randomColor4);
+    
+    }
+randomColor4();
+
